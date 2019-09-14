8a24c8fc1241dc8585d98928de04475faaafdf0e
IzaakWN <iwn_@hotmail.com>
2019-09-14 16:11:20 +0200

    columnize status

--- status.py
+++ status.py
@@ -6,6 +6,8 @@ from commands import connectClimateChamber, executeSimServCmd, unpackSimServData
                      getRunStatus, checkActiveWarnings, openActiveWarnings,\
                      getTemp, getSetp, getDewp, getAir, getDryer
 
+def addRow(string,col1,col2,just=26):
+  return string + '\n  ' + col1.ljust(just) + '  ' + col2.ljust(just)
 
 def getCurrentStatus(**kwargs):
   """Get current status."""
@@ -20,6 +22,7 @@ def getCurrentStatus(**kwargs):
   client = connectClimateChamber(ip=ip)
   
   # WRITE STATUS
+  print "Checking status..."
   with open(logname,'w+') as logfile:
     tnow = datetime.datetime.now()
     if client==None:
@@ -28,16 +31,17 @@ def getCurrentStatus(**kwargs):
       string += "\n  time stamp:  %s"%(tnow.strftime(tformat))
     else:
       string  = "Climate chamber's currect status: %s"%(getRunStatus(client))
-      string += "\n  IP address:  %s"%(ip)
-      string += "\n  time stamp:  %s"%(tnow.strftime(tformat))
-      string += "\n  setpoint:    %8.3f"%(getSetp(client))
-      string += "\n  temperature: %8.3f"%(getTemp(client))
-      string += "\n  dewpoint:    %8.3f"%(getDewp(client))
-      string += "\n  compr. air:  %4s"%('ON' if getAir(client)==1 else 'OFF')
-      string += "\n  dryer:       %4s"%('ON' if getDryer(client)==1 else 'OFF')
-      string += "\n  alarms:      %4d"%(checkActiveWarnings(client,type=1))
-      string += "\n  warnings:    %4d"%(checkActiveWarnings(client,type=2))
-      string += "\n  messages:    %4d"%(checkActiveWarnings(client,type=4))
+      addRow(string,"IP address:  %s"%(ip),
+                    "compr. air:  %4s"%('ON' if getAir(client)==1 else 'OFF'))
+      addRow(string,"time stamp:  %s"%(tnow.strftime(tformat)),
+                    "dryer:       %4s"%('ON' if getDryer(client)==1 else 'OFF'))
+      addRow(string,"setpoint:    %8.3f"%(getSetp(client)),
+                    "alarms:      %4d"%(checkActiveWarnings(client,type=1)))
+      addRow(string,"temperature: %8.3f"%(getTemp(client)),
+                    "warnings:    %4d"%(checkActiveWarnings(client,type=2)))
+      addRow(string,"dewpoint:    %8.3f"%(getDewp(client)),
+                    "messages:    %4d"%(checkActiveWarnings(client,type=4)))
+    print "Writing to '%s'"%logname
     print string
     logfile.write(string)   
   print "Status check finished!"
