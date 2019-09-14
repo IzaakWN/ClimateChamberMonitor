f046a60dce94b9f3e214c76341b6f684ceeac327
IzaakWN <iwn_@hotmail.com>
2019-09-14 20:07:58 +0200

    write messages to separate log files

--- status.py
+++ status.py
@@ -7,8 +7,34 @@ from commands import connectClimateChamber, executeSimServCmd, unpackSimServData
                      getRunStatus, checkActiveWarnings, openActiveWarnings,\
                      getTemp, getSetp, getDewp, getAir, getDryer
 
+
 def addRow(col1,col2,just=38):
-  return '\n    ' + col1.ljust(just) + '    ' + col2.ljust(just)
+  return '\n    ' + col1.ljust(just) + '  ' + col2.ljust(just)
+  
+
+def addTag(logname,tag):
+  """Add tag before file extension."""
+  if '_'!=tag[0]:
+    tag = '_'+tag
+  if '.' in logname:
+    parts = logname.split('.')
+    parts[-2] += tag
+    logname = '.'.join(parts)
+  else:
+    logname = logname + tag
+  return logname
+  
+
+def writeMessages(logname,messages,tag=""):
+  """Write messages to file."""
+  if tag:
+    logname = addTag(logname,tag)
+  with open(logname,'w+') as logfile:
+    tnow = datetime.datetime.now()
+    logfile.write(tnow.strftime('%d-%m-%Y %H:%M:%S'))
+    for message in messages:
+      logfile.write('\n'+message)
+  
 
 def getCurrentStatus(**kwargs):
   """Get current status."""
@@ -22,30 +48,55 @@ def getCurrentStatus(**kwargs):
   print "Connecting to climate chamber..."
   client = connectClimateChamber(ip=ip)
   
-  # WRITE STATUS
+  # GET STATUS
   print "Checking status..."
-  with open(logname,'w+') as logfile:
-    tnow = datetime.datetime.now()
-    if client==None:
-      string  = "  Climate chamber not found in network."
-      string += "\n    IP address:  %s"%(ip)
-      string += "\n    time stamp:  %s"%(tnow.strftime(tformat))
-    else:
-      string  = "  Climate chamber's currect status: %s"%(getRunStatus(client))
-      string += addRow("IP address:  %s"%(ip),
-                       "compr. air:  %4s"%('ON' if getAir(client)==1 else 'OFF'))
-      string += addRow("time stamp:  %s"%(tnow.strftime(tformat)),
-                       "dryer:       %4s"%('ON' if getDryer(client)==1 else 'OFF'))
-      string += addRow("setpoint:    %8.3f"%(getSetp(client)),
-                       "alarms:      %4d"%(checkActiveWarnings(client,type=1)))
-      string += addRow("temperature: %8.3f"%(getTemp(client)),
-                       "warnings:    %4d"%(checkActiveWarnings(client,type=2)))
-      string += addRow("dewpoint:    %8.3f"%(getDewp(client)),
-                       "messages:    %4d"%(checkActiveWarnings(client,type=4)))
-    print "Writing to '%s'"%logname
-    print string
-    logfile.write(string)   
+  tnow = datetime.datetime.now()
+  nalarms, nwarns, nmsgs = 0, 0, 0
+  if client==None:
+    string  = "  Climate chamber not found in network."
+    string += addRow("IP address:  %s"%(ip),
+                     "compr. air:  ")
+    string += addRow("time stamp:  %s"%(tnow.strftime(tformat)),
+                     "dryer:       ")
+    string += addRow("setpoint:    ",
+                     "alarms:      ")
+    string += addRow("temperature: ",
+                     "warnings:    ")
+    string += addRow("dewpoint:    ",
+                     "messages:    ")
+  else:
+    nalarms = checkActiveWarnings(client,type=1)
+    nwarns  = checkActiveWarnings(client,type=2)
+    nmsgs   = checkActiveWarnings(client,type=4)
+    string  = "  Climate chamber's currect status: %s"%(getRunStatus(client))
+    string += addRow("IP address:  %s"%(ip),
+                     "compr. air:  %4s"%('ON' if getAir(client)==1 else 'OFF'))
+    string += addRow("time stamp:  %s"%(tnow.strftime(tformat)),
+                     "dryer:       %4s"%('ON' if getDryer(client)==1 else 'OFF'))
+    string += addRow("setpoint:    %8.3f"%(getSetp(client)),
+                     "alarms:      %4d"%nalarms)
+    string += addRow("temperature: %8.3f"%(getTemp(client)),
+                     "warnings:    %4d"%nwarns)
+    string += addRow("dewpoint:    %8.3f"%(getDewp(client)),
+                     "messages:    %4d"%nmsgs)
   print "Status check finished!"
+  
+  # WRITE STATUS
+  if logname:
+    with open(logname,'w+') as logfile:
+      print "Writing to '%s'"%logname
+      print string
+      print "Writing status to '%s'..."%logname
+      logfile.write(string)
+    if nalarms>0:
+      alarms   = getActiveWarnings(client,type=1)
+      writeMessages(logname,messages,tag="alarms")
+    if nwarns>0:
+      warnings = getActiveWarnings(client,type=2)
+      writeMessages(logname,messages,tag="warnings")
+    if nmsgs>0:
+      messages = getActiveWarnings(client,type=4)
+      writeMessages(logname,messages,tag="messages")
   
   # DISCONNECT
   print "Closing connection..."
