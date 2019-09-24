06fa13b646a3c6a6330ba927272685ef5d2d400e
IzaakWN <iwn_@hotmail.com>
2019-09-24 18:32:58 +0200

    use class for adding custom functions

--- commands.py
+++ commands.py
@@ -153,7 +153,14 @@ getAir   = lambda c: int(executeSimServCmd(c,'GET DIGI_OUT VAL',[7])[0])
 getDryer = lambda c: int(executeSimServCmd(c,'GET DIGI_OUT VAL',[8])[0])
 startRun = lambda c: executeSimServCmd(client,'START MANUAL',[1,1])
 stopRun  = lambda c: executeSimServCmd(client,'START MANUAL',[1,0])
-
+class ClimateChamber(_socketobject):
+  def getDewp(self):  return getDewp(self)
+  def getTemp(self):  return getTemp(self)
+  def getSetp(self):  return getSetp(self)
+  def getAir(self):   return getAir(self)
+  def getDryer(self): return getDryer(self)
+  def startRun(self): return startRun(self)
+  def stopRun(self):  return stopRun(self)
 
 
 def executeSimServCmd(client, cmdstr, args=[ ], chamber=1, verbose=False):
@@ -207,14 +214,7 @@ def connectClimateChamber(ip='130.60.164.144',port=2049):
   """Connect to climate chamber via give IP address."""
   client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # create stream socket
   result = client.connect((ip,port)) # connect to protocol server
-  if client:
-    setattr(client,'getDewp',  getDewp)
-    setattr(client,'getTemp',  getTemp)
-    setattr(client,'getSetp',  getSetp)
-    setattr(client,'getAir',   getAir)
-    setattr(client,'getDryer', getDryer)
-    setattr(client,'startRun', startRun)
-    setattr(client,'stopRun',  stopRun)
+  client.__class_ = ClimateChamber
   return client
   
 
