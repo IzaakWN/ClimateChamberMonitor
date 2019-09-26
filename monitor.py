86f9160eca535f77f4b39101973f0f5e3612db68
IzaakWN <iwn_@hotmail.com>
2019-09-26 22:28:36 +0200

    debug

--- monitor.py
+++ monitor.py
@@ -165,12 +165,12 @@ def monitor(chamber,ymeteo1=None,ymeteo2=None,**kwargs):
       #axis2.set_xlabel("Time",fontsize=16)
       axis2.set_ylabel("Temperature [$^\circ$C]",fontsize=16)
       axis2.grid(axis='x',which='minor',linewidth=0.2)
-      axis2.grid(axis='x',which='major',color='darkred',linewidth=0.4,linestyle='--')
+      axis2.grid(axis='x',which='major',linewidth=0.5,color='darkred',linestyle='--')
       axis2.grid(axis='y',which='major',linewidth=0.2)
       setpline, = axis2.plot(tvals,setpvals,color='darkgrey',marker='.',label="Target temp.",linewidth=0.5,markersize=4)
       templine_YM1, = axis2.plot(tvals,tempvals_YM1,'--',color='blue',label="Temp. YM1",linewidth=0.5) #,marker='^',markersize=1
       templine_YM2, = axis2.plot(tvals,tempvals_YM2,'--',color='limegreen',label="Temp. YM2",linewidth=0.5) #,marker='v',markersize=1
-      templine, = axis2.plot(tvals,tempvals,color='red',marker='o',label="Temperature",linewidth=2,markersize=4)
+      templine,     = axis2.plot(tvals,tempvals,color='red',marker='o',label="Temperature",linewidth=2,markersize=4)
       dewpline_YM1, = axis2.plot(tvals,dewpvals_YM1,color='blue',marker='^',label="Dewpoint YM1",linewidth=1,markersize=5)
       dewpline_YM2, = axis2.plot(tvals,dewpvals_YM2,color='limegreen',marker='v',label="Dewpoint YM2",linewidth=1,markersize=4)
       legorder = [templine,setpline,templine_YM1,templine_YM2,dewpline_YM1,dewpline_YM2]
