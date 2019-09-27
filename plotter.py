1fdade599b48141625a48eb1bb493b585da8c396
IzaakWN <iwn_@hotmail.com>
2019-09-27 12:29:04 +0200

    transparent legend background

--- plotter.py
+++ plotter.py
@@ -140,7 +140,7 @@ def plotter(**kwargs):
   dewpline_YM1, = axis2.plot(tvals,dewpvals_YM1,color='blue',marker='^',label="Dewpoint YM1",linewidth=1,markersize=5)
   dewpline_YM2, = axis2.plot(tvals,dewpvals_YM2,color='limegreen',marker='v',label="Dewpoint YM2",linewidth=1,markersize=4)
   legorder = [templine,setpline,templine_YM1,templine_YM2,dewpline_YM1,dewpline_YM2]
-  axis2.legend(legorder,[l.get_label() for l in legorder],loc='upper left',framealpha=0,fontsize=13)
+  axis2.legend(legorder,[l.get_label() for l in legorder],loc='upper left',framealpha=0.5,fontsize=13)
   
   #fig.canvas.draw()
   #plt.show(block=True)
